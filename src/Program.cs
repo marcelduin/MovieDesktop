d6e852760109da29b13b94e8869fd2fc512fe3de
Marcel Duin <marcel@q42.nl>
2018-08-02 10:35:11 +0200

    video now renders behind desktop icons + added debug sample video

--- src/Program.cs
+++ src/Program.cs
@@ -9,28 +9,26 @@ namespace MovieDesktop
 {
   class Player : Form
   {
-    [DllImport("user32.dll", SetLastError = true)]
-    static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
-    [DllImport("user32.dll", SetLastError = true)]
-    static extern IntPtr GetShellWindow();
-
     private VlcControl video;
 
     [STAThread]
     static void Main(string[] args)
     {
+#if DEBUG
+      // Sample video on first screen
+      Application.Run(new Player("https://i.imgur.com/VMb5aPE.mp4", 0));
+#else
       if (args.Length < 1) throw new ArgumentException("No input file or url given");
-
       uint screenNum = 1;
       if (args.Length == 2) UInt32.TryParse(args[1], out screenNum);
 
       Application.Run(new Player(args[0], screenNum-1));
+#endif
     }
-
 
     public Player(string videoSrc, uint screenIdx = 0)
     {
-      var desktop = GetShellWindow();
+      var desktop = GetWorkerW();
 
       if(desktop == IntPtr.Zero)
       {
@@ -119,6 +117,97 @@ namespace MovieDesktop
       });
 
     }
+
+    // Imports
+    [Flags]
+    enum SendMessageTimeoutFlags : uint
+    {
+      SMTO_NORMAL = 0x0,
+      SMTO_BLOCK = 0x1,
+      SMTO_ABORTIFHUNG = 0x2,
+      SMTO_NOTIMEOUTIFNOTHUNG = 0x8,
+      SMTO_ERRORONEXIT = 0x20
+    }
+    [DllImport("User32", SetLastError = true)]
+    static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    static extern IntPtr SendMessageTimeout(
+    IntPtr windowHandle,
+    uint Msg,
+    IntPtr wParam,
+    IntPtr lParam,
+    SendMessageTimeoutFlags flags,
+    uint timeout,
+    out IntPtr result);
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+    private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+    [DllImport("User32", SetLastError = true)]
+    static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+    /// <summary>
+    /// Get processId of actual desktop background renderer
+    /// From https://www.codeproject.com/Articles/856020/Draw-behind-Desktop-Icons-in-Windows
+    /// </summary>
+    /// <returns>IntPtr to WorkerW processId</returns>
+
+    private static IntPtr GetWorkerW()
+    {
+
+      // Fetch the Progman window
+      IntPtr progman = FindWindow("Progman", null);
+
+#if DEBUG
+      Console.WriteLine("progman process: " + progman);
+#endif
+
+      IntPtr result = IntPtr.Zero;
+
+      // Send 0x052C to Progman. This message directs Progman to spawn a
+      // WorkerW behind the desktop icons. If it is already there, nothing
+      // happens.
+      SendMessageTimeout(progman,
+                             0x052C,
+                             new IntPtr(0),
+                             IntPtr.Zero,
+                             SendMessageTimeoutFlags.SMTO_NORMAL,
+                             1000,
+                             out result);
+
+      IntPtr workerw = IntPtr.Zero;
+
+      // We enumerate all Windows, until we find one, that has the SHELLDLL_DefView
+      // as a child.
+      // If we found that window, we take its next sibling and assign it to workerw.
+      EnumWindows(new EnumWindowsProc((tophandle, topparamhandle) =>
+      {
+        IntPtr p = FindWindowEx(tophandle,
+                                    IntPtr.Zero,
+                                    "SHELLDLL_DefView",
+                                    "");
+
+        if (p != IntPtr.Zero)
+        {
+          // Gets the WorkerW Window after the current one.
+          workerw = FindWindowEx(IntPtr.Zero,
+                                     tophandle,
+                                     "WorkerW",
+                                     "");
+        }
+
+        return true;
+      }), IntPtr.Zero);
+
+#if DEBUG
+      Console.WriteLine("workerw process: " + workerw);
+#endif
+
+      return workerw;
+    }
+
 
   }
 }
